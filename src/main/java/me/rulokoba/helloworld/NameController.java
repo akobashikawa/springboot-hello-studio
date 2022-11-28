946ff0fff67bc5e2dcd98417b2d34d4b9ed11957
Antonio Kobashikawa <akobashikawa@gmail.com>
2022-11-27 22:24:49 -0500

    Agrega ejemplos GET
    GET simple
    GET/{name}
    GET?name=aaa

--- src/main/java/me/rulokoba/helloworld/NameController.java
+++ src/main/java/me/rulokoba/helloworld/NameController.java
@@ -1,6 +1,12 @@
 package me.rulokoba.helloworld;
 
+import java.util.Optional;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,6 +14,25 @@ public class NameController {
 
 	@GetMapping("")
 	public String home() {
+		return "Hello World Studio";
+	}
+	
+//	@RequestMapping(value="/hello-world", method=RequestMethod.GET)
+	@GetMapping("/hello-world")
+	public String hello() {
 		return "Hello World";
+	}
+	
+	@GetMapping(value = {"/hello-name", "/hello-name/{name}"})
+	public String helloName(@PathVariable(required = false) String namePar) {
+//	public String helloName(@PathVariable Optional<String> namePar) {
+		String name = namePar != null ? namePar : "Anonymous";
+//		String name = namePar.isPresent() ? namePar.get() : "Anonymous";
+		return "Hello " + name;
+	}
+	
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "Anonymous") String name) {
+		return "Hello " + name;
 	}
 }
