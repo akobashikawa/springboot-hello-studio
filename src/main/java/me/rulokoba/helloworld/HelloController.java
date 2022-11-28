3b84f7c3247a8aed1651f60d202dca71d7a4e5df
Antonio Kobashikawa <akobashikawa@gmail.com>
2022-11-27 23:40:11 -0500

    Renombra controller

--- src/main/java/me/rulokoba/helloworld/HelloController.java
+++ src/main/java/me/rulokoba/helloworld/HelloController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-public class NameController {
+public class HelloController {
 
 	@GetMapping("")
 	public String home() {
